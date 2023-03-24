bd62c1741e416a1b41683fd212972191c17c19f3
Sam11ra <samiraoharakat@gmail.com>
2023-03-23 18:22:21 -0700

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,7 +7,7 @@
  */
 void print_most_numbers(void)
 {
-	int c;
+	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
@@ -16,5 +16,5 @@ void print_most_numbers(void)
 		else
 			_putchar(i + '0');
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
